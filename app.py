72b764ea3784bf87139bb32d1a6507888cb109c4
Irina Kim <irinahpda@gmail.com>
2020-09-08 22:53:51 +0100

    replace cursor with db call.

--- app.py
+++ app.py
@@ -22,14 +22,14 @@ app = Flask(__name__)
 #################################################
 # Database Setup
 #################################################
-#### test irina
-con = psycopg2.connect(database="hurricanes_db", user="postgres", password=password, host="127.0.0.1", port="5432")
-cursor = con.cursor()
-# Connect to the local database
-connection_string = f'{username}:{password}@localhost:5432/hurricanes_db'
-engine = create_engine(f'postgresql://{connection_string}')
+# #### test irina
+# con = psycopg2.connect(database="hurricanes_db", user="postgres", password=password, host="127.0.0.1", port="5432")
+# cursor = con.cursor()
+# # Connect to the local database
+# connection_string = f'{username}:{password}@localhost:5432/hurricanes_db'
+# engine = create_engine(f'postgresql://{connection_string}')
 
-##### end test irina
+# ##### end test irina
 
 from flask_sqlalchemy import SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
@@ -47,8 +47,9 @@ Classes = create_classes(db)
 def jsondata():
     # cursor.execute("select row_to_json(master) from master")
     # rows = cursor.fetchall()
-    cursor.execute("select name, hurricane_id, year, latitude_decimal, longitude_decimal, max_wind, air_pressure, time from master")
-    rows = cursor.fetchall()
+    # cursor.execute("select name, hurricane_id, year, latitude_decimal, longitude_decimal, max_wind, air_pressure, time from master")
+    # rows = cursor.fetchall()
+    row = db.session.query(master.name, master.hurricane_id, master.year, master.latitude_decimal, master.longitude_decimal, master.max_wind, master.air_pressure)
     # print(rows)
     # q = ("select row_to_json(master) from master")
     # mySQL = db.executesql(q)
@@ -74,113 +75,113 @@ def jsondata():
     # return render_template("jsondata.html", data=j)    
     return j
 
-# create route that renders index.html template
-@app.route("/maxwinds")
-def maxwinds():
-    # cursor.execute("select row_to_json(master) from master")
-    # rows = cursor.fetchall()
-    cursor.execute("select name, max_wind, name_year from maxwinds")
-    rows = cursor.fetchall()
-    # print(rows)
-    # q = ("select row_to_json(master) from master")
-    # mySQL = db.executesql(q)
-    # return json.dumps(mySQL)
-    # # return 
-    objects_list = []
-    for row in rows:
-        d = collections.OrderedDict()
-        d['name'] = row[0]
-        d['max_wind'] = row[1]
-        d['name_year'] = row[2]
-        objects_list.append(d)
+# # create route that renders index.html template
+# @app.route("/maxwinds")
+# def maxwinds():
+#     # cursor.execute("select row_to_json(master) from master")
+#     # rows = cursor.fetchall()
+#     cursor.execute("select name, max_wind, name_year from maxwinds")
+#     rows = cursor.fetchall()
+#     # print(rows)
+#     # q = ("select row_to_json(master) from master")
+#     # mySQL = db.executesql(q)
+#     # return json.dumps(mySQL)
+#     # # return 
+#     objects_list = []
+#     for row in rows:
+#         d = collections.OrderedDict()
+#         d['name'] = row[0]
+#         d['max_wind'] = row[1]
+#         d['name_year'] = row[2]
+#         objects_list.append(d)
 
-    j = json.dumps(objects_list)
-    objects_file = 'maxwinds.js'
-    f = open(objects_file,'w')
-    # print(f, j)
-    # return render_template("maxwinds.html", data=j)    
-    return j
+#     j = json.dumps(objects_list)
+#     objects_file = 'maxwinds.js'
+#     f = open(objects_file,'w')
+#     # print(f, j)
+#     # return render_template("maxwinds.html", data=j)    
+#     return j
 
-# create route that renders index.html template
-@app.route("/cost_by_state")
-def cost_by_state():
-    # cursor.execute("select row_to_json(master) from master")
-    # rows = cursor.fetchall()
-    cursor.execute("select state, total_damage from cost_state")
-    rows = cursor.fetchall()
-    # print(rows)
-    # q = ("select row_to_json(master) from master")
-    # mySQL = db.executesql(q)
-    # return json.dumps(mySQL)
-    # # return 
-    objects_list = []
-    for row in rows:
-        d = collections.OrderedDict()
-        d['name'] = row[0]
-        d['total_damage'] = row[1]
-        objects_list.append(d)
+# # create route that renders index.html template
+# @app.route("/cost_by_state")
+# def cost_by_state():
+#     # cursor.execute("select row_to_json(master) from master")
+#     # rows = cursor.fetchall()
+#     cursor.execute("select state, total_damage from cost_state")
+#     rows = cursor.fetchall()
+#     # print(rows)
+#     # q = ("select row_to_json(master) from master")
+#     # mySQL = db.executesql(q)
+#     # return json.dumps(mySQL)
+#     # # return 
+#     objects_list = []
+#     for row in rows:
+#         d = collections.OrderedDict()
+#         d['name'] = row[0]
+#         d['total_damage'] = row[1]
+#         objects_list.append(d)
 
-    j = json.dumps(objects_list)
-    objects_file = 'cost_by_state.js'
-    f = open(objects_file,'w')
-    # print(f, j)
-    # return render_template("cost_by_state.html", data=j)    
-    return j
+#     j = json.dumps(objects_list)
+#     objects_file = 'cost_by_state.js'
+#     f = open(objects_file,'w')
+#     # print(f, j)
+#     # return render_template("cost_by_state.html", data=j)    
+#     return j
 
-# create route that renders index.html template
-@app.route("/")
-def home():
-    return render_template("index.html")
+# # create route that renders index.html template
+# @app.route("/")
+# def home():
+#     return render_template("index.html")
   
-# create route that renders index.html template
-@app.route("/data")
-def data():
-    cursor.execute("select * from master")
-    results = cursor.fetchall()
-    return render_template("data.html", data=results)
+# # create route that renders index.html template
+# @app.route("/data")
+# def data():
+#     cursor.execute("select * from master")
+#     results = cursor.fetchall()
+#     return render_template("data.html", data=results)
     
 
-# # Query the database and send the jsonified results
-# @app.route("/send", methods=["GET", "POST"])
-# def send():
-#     if request.method == "POST":
-#         name = request.form["petName"]
-#         lat = request.form["petLat"]
-#         lon = request.form["petLon"]
+# # # Query the database and send the jsonified results
+# # @app.route("/send", methods=["GET", "POST"])
+# # def send():
+# #     if request.method == "POST":
+# #         name = request.form["petName"]
+# #         lat = request.form["petLat"]
+# #         lon = request.form["petLon"]
 
-#         pet = Pet(name=name, lat=lat, lon=lon)
-#         db.session.add(pet)
-#         db.session.commit()
-#         return redirect("/", code=302)
+# #         pet = Pet(name=name, lat=lat, lon=lon)
+# #         db.session.add(pet)
+# #         db.session.commit()
+# #         return redirect("/", code=302)
 
-#     return render_template("form.html")
+# #     return render_template("form.html")
 
 
-# @app.route("/api/pals")
-# def pals():
-#     results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
+# # @app.route("/api/pals")
+# # def pals():
+# #     results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
 
-#     hover_text = [result[0] for result in results]
-#     lat = [result[1] for result in results]
-#     lon = [result[2] for result in results]
+# #     hover_text = [result[0] for result in results]
+# #     lat = [result[1] for result in results]
+# #     lon = [result[2] for result in results]
 
-#     pet_data = [{
-#         "type": "scattergeo",
-#         "locationmode": "USA-states",
-#         "lat": lat,
-#         "lon": lon,
-#         "text": hover_text,
-#         "hoverinfo": "text",
-#         "marker": {
-#             "size": 50,
-#             "line": {
-#                 "color": "rgb(8,8,8)",
-#                 "width": 1
-#             },
-#         }
-#     }]
+# #     pet_data = [{
+# #         "type": "scattergeo",
+# #         "locationmode": "USA-states",
+# #         "lat": lat,
+# #         "lon": lon,
+# #         "text": hover_text,
+# #         "hoverinfo": "text",
+# #         "marker": {
+# #             "size": 50,
+# #             "line": {
+# #                 "color": "rgb(8,8,8)",
+# #                 "width": 1
+# #             },
+# #         }
+# #     }]
 
-#     return jsonify(pet_data)
+# #     return jsonify(pet_data)
 
 
 if __name__ == "__main__":
