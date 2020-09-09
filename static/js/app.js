040bc359e590687931e91b8e8cf39007e3bffc39
Irina Kim <irinahpda@gmail.com>
2020-09-09 13:11:15 +0100

    Tried to create a base map - still does not plot.

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,11 @@
 function buildGraph(sample) {
   const url = "/maxwinds";
   d3.json(url).then(function(data){
-    console.log(data);
+    // console.log(data);
     const names = data.map(entry => entry.name_year);
     const maxwind = data.map(entry => entry.max_wind);
-    console.log(names);
-    console.log(maxwind);    
+    // console.log(names);
+    // console.log(maxwind);    
  
     const title = `Maximum winds`;
     const trace = {
@@ -45,10 +45,31 @@ buildGraph();
 
 function geoJsonMap(sample) {
   const url = "/jsondata";
-  d3.json(url).then(function(data){
-    console.log(data);
-    const myMap = d3.select("#geoJsonMap");
+  const url2 = "https://leafletjs.com/examples/choropleth/us-states.js"
+  // d3.json(url2).then(function(data){
+    // console.log(data);
+    // const myMap = L.map("geoJsonMap", {
+    //   center: [29.75, -95.36],
+    //   zoom: 4
+    // });
+    d3.json(url2).then(function(data){
+      console.log(data);
     
+    // Adding a tile layer (the background map image) to our map
+    // We use the addTo method to add objects to our map
+    var mapboxAccessToken = "pk.eyJ1IjoiaXJpbmFocGRhIiwiYSI6ImNrZTk5ZWl6czB0ZGIyeHJucnBidXF4NG0ifQ.5yKGPM6y8H8d_d_Gj8KAVw";
+
+    var map = L.map('geoJsonMap').setView([37.8, -96], 4);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + mapboxAccessToken, {
+        id: 'mapbox/light-v9',
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        zoomOffset: -1
+    }).addTo(map);
+
+    L.geoJson(statesData).addTo(map);
+
   })
 };
 
