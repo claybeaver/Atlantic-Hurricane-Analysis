e220584e443fbd9caa9af7eb0ed6b70fa7846353
Irina Kim <irinahpda@gmail.com>
2020-09-09 15:07:47 +0100

    minor changes

--- static/js/app.js
+++ static/js/app.js
@@ -44,33 +44,35 @@ function buildGraph(sample) {
 buildGraph();
 
 function geoJsonMap(sample) {
-  const url = "/jsondata";
-  const url2 = "https://leafletjs.com/examples/choropleth/us-states.js"
+  const url = "/cost_by_state";
   // d3.json(url2).then(function(data){
     // console.log(data);
     // const myMap = L.map("geoJsonMap", {
     //   center: [29.75, -95.36],
     //   zoom: 4
     // });
-    d3.json(url2).then(function(data){
+    d3.json(url).then(function(data){
       console.log(data);
     
     // Adding a tile layer (the background map image) to our map
     // We use the addTo method to add objects to our map
-    var mapboxAccessToken = API_KEY;
+    // var mapboxAccessToken = API_KEY;
 
-    var map = L.map('geoJsonMap').setView([37.8, -96], 4);
+    // var map = L.map('geoJsonMap').setView([37.8, -96], 4);
 
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + mapboxAccessToken, {
-        id: 'mapbox/light-v9',
+    const map = L.map("geoJsonMap", {
+      center: [29.75, -95.36],
+      zoom: 13
+    });
+
+    L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        maxZoom: 5,
         tileSize: 512,
+        id: 'mapbox/streets-v11',
         zoomOffset: -1,
-        API_KEY
+        accessToken: API_KEY
     }).addTo(map);
-
-    L.geoJson(statesData).addTo(map);
-
   })
 };
 
