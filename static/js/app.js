d64fa184ae5a90ceff9c5e8f6b6fcf4c9dc87e47
Irina Kim <irinahpda@gmail.com>
2020-09-12 04:28:51 +0100

    Modify trademar to try to fix issue on heroku.

--- static/js/app.js
+++ static/js/app.js
@@ -215,7 +215,7 @@ amyChart();
 const top10 = async() => {
    // here below this line is the code for Neil
    const data = await (await fetch("/top10")).json();
-   // console.log(data);
+   console.log(data);
    var lat_lon = data.map(entry => entry.lat_lon);
    var name = data.map(entry => entry.name);
    var name_year = data.map(entry => entry.name_year);
@@ -234,7 +234,7 @@ const top10 = async() => {
 
    
    // Create an initial map object
-   const myMap = L.map("top10").setView([25.07, -70.1], 4);
+   const myTop10Map = L.map("top10").setView([25.07, -70.1], 4);
 
    //  // Add a tile layer to map
    //  const dark = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -244,11 +244,12 @@ const top10 = async() => {
    //  }).addTo(myMap);
 
    const light = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
       id: "light-v10",
       accessToken: API_KEY
-      }).addTo(myMap);
+      }).addTo(myTop10Map);
+
 
     // // *************** EXAMPLE MAPPING ABLE_1950 *****************
    //  var hurricane = object.Able_1950
