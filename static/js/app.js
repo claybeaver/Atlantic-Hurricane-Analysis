bb905554939dc039dc9b2afab8311fcb87c6b101
Neil Vora <nvora@chemium.local>
2020-09-12 01:14:14 -0500

    Added chart.js labels

--- static/js/app.js
+++ static/js/app.js
@@ -44,11 +44,25 @@ var scatterChart = new Chart(ctx, {
       }]
    },
    options: {
+      scales: {
+         xAxes: [{
+            scaleLabel: {
+               display: true,
+               labelString: 'Damage Cost in USD (Billions)'
+            }
+         }],
+         yAxes: [{
+            scaleLabel: {
+               display: true,
+               labelString: 'Maximum Wind Speed (MPH)'
+            }
+         }]
+      },
       tooltips: {
          callbacks: {
             label: function(tooltipItem, data) {
                var label = data.labels[tooltipItem.index];
-               return label + ': (' + tooltipItem.xLabel + ', ' + tooltipItem.yLabel + ')';
+               return label + ': (' + tooltipItem.xLabel + ' Billion USD, ' + tooltipItem.yLabel + ' MPH)';
             }
          }
       }
@@ -100,7 +114,7 @@ data.forEach((entry, index) => {
       names_years.push(new_name_year);
    }
 })
-
+console.log(object)
 
     // Create an initial map object
     const myMap = L.map("geomap").setView([25.07, -70.1], 4);
@@ -120,7 +134,7 @@ data.forEach((entry, index) => {
       }).addTo(myMap);
 
     // // *************** EXAMPLE MAPPING ABLE_1950 *****************
-    var hurricane = object.Katrina_2005
+    var hurricane = object
     var line = hurricane.Coordinates
    //  console.log(line)
 
@@ -203,6 +217,69 @@ Plotly.newPlot("costmap", chartData, layout, {showLink: false});
 
 amyChart();
 
+
+// ************************************************************************************************************************************************
+// ************************************************************************************************************************************************
+// *************************************************                                    ***********************************************************
+// *************************************************            TOP 10 GEOMAP           ***********************************************************
+// *************************************************                                    ***********************************************************
+// ************************************************************************************************************************************************
+// ************************************************************************************************************************************************
+
+const top10 = async() => {
+   // here below this line is the code for Neil
+   const data = await (await fetch("/top10")).json();
+   console.log(data);
+   var lat_lon = data.map(entry => entry.lat_lon);
+   var name = data.map(entry => entry.name);
+   var name_year = data.map(entry => entry.name_year);
+   var hurricane_id = data.map(entry => parseInt(entry.hurricane_id));
+   var max_wind = data.map(entry => parseInt(entry.max_wind));
+   var air_pressure = data.map(entry => parseInt(entry.air_pressure));
+   var time = data.map(entry => parseInt(entry.time));
+   var lat_lon = data.map(entry => entry.lat_lon);
+   var norm_damage_usd = data.map(entry => entry.norm_damage_usd);
+   var damage_usd = data.map(entry => entry.damage_usd);
+   
+   // console.log(damage_usd);
+
+   // var costs = data.map(entry => parseInt(entry.total_damage))
+
+
+   
+   // Create an initial map object
+   const myTop10Map = L.map("top10").setView([25.07, -70.1], 4);
+
+   //  // Add a tile layer to map
+   //  const dark = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+   //    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+   //    id: "dark-v10",
+   //    accessToken: API_KEY
+   //  }).addTo(myMap);
+
+   const light = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: "light-v10",
+      accessToken: API_KEY
+      }).addTo(myTop10Map);
+
+
+    // // *************** EXAMPLE MAPPING ABLE_1950 *****************
+   //  var hurricane = object.Able_1950
+   //  var line = hurricane.Coordinates
+   //  console.log(line)
+
+    // // **********************************************************
+
+    // Create a polyline using the line coordinates and pass in some initial options
+   //  L.polyline(line, {
+   //    color: "red"
+   //  }).addTo(myMap);
+
+}
+
+top10();
 
 // ************************************************************************************************************************************************
 // ************************************************************************************************************************************************
